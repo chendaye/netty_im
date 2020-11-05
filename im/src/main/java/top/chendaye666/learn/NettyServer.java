df097e8b8d8ba5da8b3781e6c0071037f084a4e7
chendaye666 <chendaye666@gmail.com>
2020-11-06 01:22:28 +0800

    save

--- im/src/main/java/top/chendaye666/learn/NettyServer.java
+++ im/src/main/java/top/chendaye666/learn/NettyServer.java
@@ -1,7 +1,25 @@
 package top.chendaye666.learn;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
 public class NettyServer {
     public static void main(String[] args) {
+        NioEventLoopGroup bossGroup = new NioEventLoopGroup();
+        NioEventLoopGroup workGroup = new NioEventLoopGroup();
 
+        ServerBootstrap serverBootstrap = new ServerBootstrap();
+
+        serverBootstrap.group(bossGroup, workGroup)
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                    protected void initChannel(NioSocketChannel ch){
+
+                    }
+                });
+        serverBootstrap.bind(8000);
     }
 }
