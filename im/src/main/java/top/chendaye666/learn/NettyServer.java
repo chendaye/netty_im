d41d07c5eaca8e381e032b96223142f77b03b04b
chendaye666 <chendaye666@gmail.com>
2020-11-11 01:02:41 +0800

    save

--- im/src/main/java/top/chendaye666/learn/NettyServer.java
+++ im/src/main/java/top/chendaye666/learn/NettyServer.java
@@ -5,21 +5,55 @@ import io.netty.channel.ChannelInitializer;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.util.concurrent.Future;
+import io.netty.util.concurrent.GenericFutureListener;
 
+/**
+ * 首先看到，我们创建了两个NioEventLoopGroup，这两个对象可以看做是传统IO编程模型的两大线程组，
+ * bossGroup表示监听端口，accept 新连接的线程组，workerGroup表示处理每一条连接的数据读写的线程组，
+ *
+ * 接下来 我们创建了一个引导类 ServerBootstrap，这个类将引导我们进行服务端的启动工作，直接new出来开搞。
+ * 我们通过.group(bossGroup, workerGroup)给引导类配置两大线程组，这个引导类的线程模型也就定型了。
+ * 然后，我们指定我们服务端的 IO 模型为NIO，我们通过.channel(NioServerSocketChannel.class)来指定 IO 模型，
+ *
+ * 接着，我们调用childHandler()方法，给这个引导类创建一个ChannelInitializer，这里主要就是定义后续每条连接的数据读写，业务处理逻辑，
+ * ChannelInitializer这个类中，我们注意到有一个泛型参数NioSocketChannel，
+ * 这个类呢，就是 Netty 对 NIO 类型的连接的抽象，而我们前面NioServerSocketChannel也是对 NIO 类型的连接的抽象，
+ * NioServerSocketChannel和NioSocketChannel的概念可以和 BIO 编程模型中的ServerSocket以及Socket两个概念对应上
+ */
 public class NettyServer {
     public static void main(String[] args) {
         NioEventLoopGroup bossGroup = new NioEventLoopGroup();
-        NioEventLoopGroup workGroup = new NioEventLoopGroup();
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
 
         ServerBootstrap serverBootstrap = new ServerBootstrap();
-
-        serverBootstrap.group(bossGroup, workGroup)
+        serverBootstrap
+                .group(bossGroup, workerGroup)
                 .channel(NioServerSocketChannel.class)
                 .childHandler(new ChannelInitializer<NioSocketChannel>() {
-                    protected void initChannel(NioSocketChannel ch){
-
+                    protected void initChannel(NioSocketChannel ch) {
                     }
                 });
-        serverBootstrap.bind(8000);
+
+//        serverBootstrap.bind(8000);
+        bind(serverBootstrap, 8000);
+    }
+
+    /**
+     * 绑定端接直到成功
+     * @param bootstrap
+     * @param port
+     */
+    public static void bind(final ServerBootstrap bootstrap, final int port){
+        bootstrap.bind(port).addListener(new GenericFutureListener<Future<? super Void>>() {
+            public void operationComplete(Future<? super Void> future) throws Exception {
+                if (future.isSuccess()){
+                    System.out.println("绑定端口："+port+"成功！");
+                }else{
+                    System.out.println("绑定端口："+port+"失败！尝试绑定端口"+(port+1));
+                    bind(bootstrap, port + 1);
+                }
+            }
+        })
     }
 }
